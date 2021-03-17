cf4e7292421c0ec122934a8cb4926a70674651b5
Facundo Vera <martin91.vera@gmail.com>
2021-03-17 12:32:23 -0300

    EventsSearch comp, useRouter and button changes

---
+++ components/events/events-search.js
@@ -0,0 +1,47 @@
+import Button from '../ui/button';
+import classes from './events-search.module.css';
+//UseRef will help us to get the value from the select tags
+import { useRef } from 'react';
+
+const EventsSearch = props => {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  //SUBMITHANDLER
+  const submitHandler = e => {
+    e.preventDefault();
+
+    //get the values from the select inputs with ref
+    const yearSelected = yearInputRef.current.value;
+    const monthSelected = monthInputRef.current.value;
+
+    //Once we click we trigger the findEventsHandler
+    //which is in the /events page (/events/index.js)
+    //passed as a prop in the EventSearch component
+    props.onSearch(yearSelected, monthSelected);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlFor="year">Year</label>
+          <select name="year" id="year" ref={yearInputRef}>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="month">Year</label>
+          <select name="month" id="month" ref={monthInputRef}>
+            <option value="January">January</option>
+            <option value="February">February</option>
+          </select>
+        </div>
+      </div>
+      <Button>Search</Button>
+    </form>
+  );
+};
+
+export default EventsSearch;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,21 @@
 import Head from 'next/head';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+import { getAllEvents } from '../../data/dummy-data';
+import { useRouter } from 'next/router';
 
 const AllEventsPage = () => {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    //this will trigger the [...slug].js
+    const fullPath = `/events/${year}/${month}`;
+
+    //push() will direct my site to the selected path
+    router.push(fullPath);
+  };
+
   return (
     <div>
       <Head>
@@ -9,7 +24,8 @@ const AllEventsPage = () => {
       </Head>
 
       <main>
-        <h2>All Events</h2>
+        <EventsSearch onSearch={findEventsHandler} />
+        <EventList items={events} />
       </main>
 
       <footer></footer>
