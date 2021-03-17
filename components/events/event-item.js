0fee0ef2b43936289e8d0b2478e5bc5eea7fbd3c
Facundo Vera <martin91.vera@gmail.com>
2021-03-17 09:18:10 -0300

    Event-list and Event-item css

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import classes from './event-item.module.css';
 
 const EventItem = props => {
   const { title, image, date, location, id } = props;
@@ -11,25 +12,25 @@ const EventItem = props => {
   });
 
   //FORMATTING THE ADDDRESS
-  const formattedAddress = location.replace(',', '\n');
+  const formattedAddress = location.replace(', ', '\n');
 
   //FORMATTING MY LINK
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={`/${image}`} alt={title} />
-      <div>
+      <div className={classes.content}>
         <div>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
