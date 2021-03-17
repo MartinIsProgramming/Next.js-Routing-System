b1fbabd7fcc170d87f6e38719cb807a298016b84
Facundo Vera <martin91.vera@gmail.com>
2021-03-17 09:31:32 -0300

    Ui folder, Button and css

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Button from '../ui/button';
 import classes from './event-item.module.css';
 
 const EventItem = props => {
@@ -31,7 +32,7 @@ const EventItem = props => {
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>Explore Event</Button>
         </div>
       </div>
     </li>
